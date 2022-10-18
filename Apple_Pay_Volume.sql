0434d95ae295a1fbf724340a3ac274b10a481585
Niiikay <85313835+Niiikay@users.noreply.github.com>
2022-10-18 11:05:32 -0500

    Add files via upload

---
+++ Apple_Pay_Volume.sql
@@ -0,0 +1,9 @@
+-- Visa
+-- Data Lemur
+-- Apple Pay Volume
+
+SELECT merchant_id, 
+SUM(CASE WHEN LOWER(payment_method) = 'apple pay' THEN (transaction_amount) ELSE 0 END) AS volume
+FROM transactions
+GROUP BY merchant_id
+ORDER BY volume DESC;
